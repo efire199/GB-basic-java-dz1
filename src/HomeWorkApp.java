0f5d2b03603df7580fa2bcde07d9b596b3e7e503
efire199 <efire199@gmail.com>
2021-08-13 18:40:07 +0300

    #repository for GB basic java

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,45 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords(){
+        System.out.println("_Orange");
+        System.out.println("_Banana");
+        System.out.println("_Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = 10;
+        int b = 5;
+        if (a + b < 0){
+            System.out.println("Сумма отрицательная");
+        }else{
+            System.out.println("Сумма положительная");
+        }
+    }
+
+    public static void printColor(){
+        int value = 100;
+        if (value<=0){
+            System.out.println("Красный");
+        }else if (value > 0 && value <=100){
+            System.out.println("Желтый");
+        }else if (value > 100){
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(){
+        int a = 5;
+        int b = 6;
+        if (a>=b){
+            System.out.println("a>=b");
+        }else{
+            System.out.println("a<b");
+        }
+    }
+}
